ad2eec0ac7a965b467f25a51c57c7819255417df
levisonjr <levisonjr21@gmail.com>
2023-09-13 21:20:12 -0300

    Get e Create implementados (DB em memória: Dictionary)

---
+++ BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,84 @@
+using BuberBreakfast.Contracts.Breakfast;
+using BuberBreakfast.Models;
+using BuberBreakfast.Services.Breakfasts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreakfastsController : ControllerBase
+{
+    private readonly IBreakfastService _breakfastService;
+
+    public BreakfastsController(IBreakfastService breakfastService)
+    {
+        _breakfastService = breakfastService;
+    }
+
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        // MODELANDO OS DADOS DA REQUISIÇÃO PARA O MODELO INTERNO
+        var breakfast  = new Breakfast(
+            Guid.NewGuid(),
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Savory,
+            request.Sweet);
+        
+        // LÓGICA PARA EXECUTAR A AÇÃO PROPOSTA PELO ENDPOINT
+        _breakfastService.CreateBreakfast(breakfast);
+
+        // CONVERTENDO OS DADOS: MODELO INTERNO -> MODELO DA API
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDateTime,
+            breakfast.Savory,
+            breakfast.Sweet);
+
+        // RETORNADO A RESPOSTA PARA O CLIENTE
+        return CreatedAtAction(
+            actionName: nameof(GetBreakfast),
+            routeValues: new { id = breakfast.Id },
+            value: response
+        );
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        Breakfast breakfast = _breakfastService.GetBreakfast(id);
+
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDateTime,
+            breakfast.Savory,
+            breakfast.Sweet);
+
+        return Ok(response);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+}
